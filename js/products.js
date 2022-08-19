0448aeafa8bc8e119a939b45c0ee4cc43e0f4808
Macarena Pereira <pereira.macarena12@gmail.com>
2022-08-19 16:18:19 -0300

    mostrar autos
    arreglado

--- js/products.js
+++ js/products.js
@@ -4,38 +4,38 @@ function showAutosList(array){
     let htmlContentToAppend = "";
 
     for(let i = 0; i < array.length; i++){ 
-        let category = array[i];
+        let cats_products = array[i];
         htmlContentToAppend += `
         <div class="list-group-item list-group-item-action">
             <div class="row">
                 <div class="col-3">
-                    <img src= "`+ product.image + `" alt="product image" class="img-thumbnail">
+                    <img src= "`+ cats_products.image + `" alt="product image" class="img-thumbnail">
                 </div>
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
                         <div class="mb-1">
-                        <h4>`+ product.name +`</h4> 
-                        <p> `+ product.description +`</p> 
+                        <h4>`+ cats_products.name +`</h4> 
+                        <p> `+ cats_products.description +`</p> 
                         </div>
-                        <small class="text-muted">` + product.currency + ` artículos</small> 
-                        <small class="text-muted">` + product.cost + ` artículos</small> 
-                        <small class="text-muted">` + product.soldCount + ` artículos</small> 
+                        <small class="text-muted">` + cats_products.currency + ` artículos</small> 
+                        <small class="text-muted">` + cats_products.cost + ` artículos</small> 
+                        <small class="text-muted">` + cats_products.soldCount + ` artículos</small> 
                     </div>
 
                 </div>
             </div>
         </div>
         `
-        document.getElementById("101").innerHTML = htmlContentToAppend; 
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
     }
 }
 
 document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(CAT_AUTOS).then(function(resultObj){
+    getJSONData(AUTOS).then(function(resultObj){
         if (resultObj.status === "ok")
         {
-            autosArray = resultObj.data;
-            showCategoriesList(autosArray);
+            autosArray = resultObj.data.products;
+            showAutosList(autosArray);
         }
     });
 });
